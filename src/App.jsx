84fab5a08bd685a58b7f2b60d7ebc126e98be19e
gift56 <efeasiughu@gmail.com>
2022-07-01 14:27:37 +0100

    moved env to root folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './component';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    console.log(products)
     return (
         <div>
             <Navbar />
