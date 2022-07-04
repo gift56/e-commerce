04c1128b08d17b895bab97239c42ab777893cec1
gift56 <efeasiughu@gmail.com>
2022-07-04 15:32:07 +0100

    adding some map in layout checkout

--- src/component/CheckoutForm/CheckOut/Checkout.jsx
+++ src/component/CheckoutForm/CheckOut/Checkout.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
+const steps = ['Shipping address', 'Payment details'];
+
+
 const Checkout = () => {
     return (
         <>
-
+            <div className={classes.toolbar} />
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography variant='h4' align='center'>Checkout</Typography>
+                    <Stepper activeStep={0} className={classes.stepper}>
+                        {steps.map((step) => (
+                            <Step key={step}>
+                                <StepLabel>{step}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Paper>
+            </main>
         </>
     )
 }
