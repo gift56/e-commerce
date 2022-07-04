4f2ea3457a76e1024e45ed518d5968ab0a4cc29c
gift56 <efeasiughu@gmail.com>
2022-07-04 15:49:05 +0100

    done adding the styes

--- src/component/CheckoutForm/CheckOut/Checkout.jsx
+++ src/component/CheckoutForm/CheckOut/Checkout.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 const steps = ['Shipping address', 'Payment details'];
 
 
 const Checkout = () => {
+    const [activeStep, setActiveStep] = useState(0);
+
+    
     return (
         <>
             <div className={classes.toolbar} />
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant='h4' align='center'>Checkout</Typography>
-                    <Stepper activeStep={0} className={classes.stepper}>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
                         {steps.map((step) => (
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
