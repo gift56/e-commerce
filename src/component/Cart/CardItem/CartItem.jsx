4fa5c4dff3f6d3ce84c2d03ade3c25c6d2714c40
gift56 <efeasiughu@gmail.com>
2022-07-04 13:49:41 +0100

    just created carditems js styling

--- src/component/Cart/CardItem/CartItem.jsx
+++ src/component/Cart/CardItem/CartItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
+import useStyle from './styles';
 
 const CartItem = () => {
+    const classes = useStyle();
     return (
         <div>
 
