6f97bd1632f0eb7b9e57a9872d301028dfa945c1
gift56 <efeasiughu@gmail.com>
2022-07-04 16:29:29 +0100

    just imported payment and adressform

--- src/component/CheckoutForm/CheckOut/Checkout.jsx
+++ src/component/CheckoutForm/CheckOut/Checkout.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 
 const steps = ['Shipping address', 'Payment details'];
-
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
