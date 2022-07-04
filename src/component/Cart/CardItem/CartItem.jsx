8dd1257e58ce4591b41de8bf3a3979686905bb78
gift56 <efeasiughu@gmail.com>
2022-07-04 14:59:14 +0100

    just finished adding the props

--- src/component/Cart/CardItem/CartItem.jsx
+++ src/component/Cart/CardItem/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 import useStyle from './styles';
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
     const classes = useStyle();
     return (
         <Card>
@@ -13,11 +13,11 @@ const CartItem = ({ item }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <div className={classes.button}>
-                    <Button type='button' size='small'>-</Button>
+                    <Button type='button' size='small' onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type='button' size='small'>+</Button>
+                    <Button type='button' size='small' onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
                 </div>
-                <Button type='button' variant='contained' color='secondary'>Remove</Button>
+                <Button type='button' variant='contained' color='secondary' onClick={() => onRemoveFromCart(item.id)}>Remove</Button>
             </CardActions>
         </Card>
     )
