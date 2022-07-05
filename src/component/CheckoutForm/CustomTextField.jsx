c801157a4b8c41ffc4961926d3948ee8cf2ebd09
gift56 <efeasiughu@gmail.com>
2022-07-05 04:06:45 +0100

    done with textfield

--- src/component/CheckoutForm/CustomTextField.jsx
+++ src/component/CheckoutForm/CustomTextField.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form/dist/useFormContext';
 
-const CustomTextField = () => {
+const CustomTextField = ({ name, label, required }) => {
     const { control } = useFormContext();
-    
+
     return (
         <Grid item xs={12} sm={6}>
-            <Controller as={TextField} control={control} />
+            <Controller as={TextField} control={control} fullWidth name={name} label={label} required={required} />
         </Grid>
     )
 }
