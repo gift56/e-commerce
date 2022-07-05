436e3a0fbf8f53733e8b0206dff7d2222034098f
gift56 <efeasiughu@gmail.com>
2022-07-05 05:05:47 +0100

    forced react hook

--- src/component/CheckoutForm/AddressForm.jsx
+++ src/component/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
-import FormInput from './CustomTextField';
+// import FormInput from './CustomTextField';
 
 console.log(InputLabel);
 console.log(Select);
@@ -15,7 +15,9 @@ const AddressForm = () => {
             <FormProvider {...methods}>
                 <form onSubmit=''>
                     <Grid container spacing={3}>
-                        <FormInput required name='firstName' label='First name' />
+                        <input name="firstName" />
+                        <input name="firstName" />
+                        <input name="firstName" />
                     </Grid>
                 </form>
             </FormProvider>
