97961d9cc9c6c93a66e1b4fcb3306043baf38f0b
gift56 <efeasiughu@gmail.com>
2022-07-05 16:36:07 +0100

    just created review component

--- src/component/CheckoutForm/PaymentForm.jsx
+++ src/component/CheckoutForm/PaymentForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Typography, Button, Divider } from '@material-ui/core';
 import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
 
 const PaymentForm = () => {
     return (
