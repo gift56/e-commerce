ad5aeb6538c94c35542084dbf6fd4534787215bf
gift56 <efeasiughu@gmail.com>
2022-07-05 17:06:18 +0100

    working on stripe

--- src/component/CheckoutForm/PaymentForm.jsx
+++ src/component/CheckoutForm/PaymentForm.jsx
@@ -4,10 +4,27 @@ import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js
 import { loadStripe } from '@stripe/stripe-js';
 import Review from './Review';
 
+const stripePromise = loadStripe('...')
+
 const PaymentForm = ({ checkoutToken }) => {
     return (
         <>
             <Review checkoutToken={checkoutToken} />
+            <Divider />
+            <Typography variant='h6' gutterBottom style={{ margin: '20px 0' }}>Payment Method</Typography>
+            <Elements stripe={stripePromise}>
+                <ElementsConsumer>
+                    {({ elements, stripe }) => (
+                        <form>
+                            <CardElement />
+                            <br /> <br />
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Button></Button>
+                            </div>
+                        </form>
+                    )}
+                </ElementsConsumer>
+            </Elements>
         </>
     )
 }
