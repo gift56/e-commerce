da1fa4922aa7d265bc85235e3a09afc5fd4b061d
gift56 <efeasiughu@gmail.com>
2022-07-05 16:34:37 +0100

    about creating review component

--- src/component/CheckoutForm/PaymentForm.jsx
+++ src/component/CheckoutForm/PaymentForm.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Typography, Button, Divider } from '@material-ui/core';
 import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const PaymentForm = () => {
     return (
-        <div>
-            PaymentForm
-        </div>
+        <>
+            <Review />
+        </>
     )
 }
 
