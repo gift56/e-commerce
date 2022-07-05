83220c64ff99d282fe1afe409118c0bd4f2d796e
gift56 <efeasiughu@gmail.com>
2022-07-05 13:25:57 +0100

    created select fileds

--- src/component/CheckoutForm/AddressForm.jsx
+++ src/component/CheckoutForm/AddressForm.jsx
@@ -3,9 +3,6 @@ import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@materia
 import { useForm, FormProvider } from 'react-hook-form';
 import FormInput from './CustomTextField';
 
-console.log(InputLabel);
-console.log(Select);
-console.log(MenuItem);
 console.log(Button);
 const AddressForm = () => {
     const methods = useForm();
@@ -15,12 +12,30 @@ const AddressForm = () => {
             <FormProvider {...methods}>
                 <form onSubmit=''>
                     <Grid container spacing={3}>
-                        <FormInput required name='firstName' label='First Name'/>
-                        <FormInput required name='lastName' label='Last Name'/>
-                        <FormInput required name='address1' label='Address'/>
-                        <FormInput required name='email' label='Email'/>
-                        <FormInput required name='city' label='City'/>
-                        <FormInput required name='zip' label='Zip / Postal code'/>
+                        <FormInput required name='firstName' label='First Name' />
+                        <FormInput required name='lastName' label='Last Name' />
+                        <FormInput required name='address1' label='Address' />
+                        <FormInput required name='email' label='Email' />
+                        <FormInput required name='city' label='City' />
+                        <FormInput required name='zip' label='Zip / Postal code' />
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select fullWidth>
+                                <MenuItem>Select Me</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select fullWidth>
+                                <MenuItem>Select Me</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select fullWidth>
+                                <MenuItem>Select Me</MenuItem>
+                            </Select>
+                        </Grid>
                     </Grid>
                 </form>
             </FormProvider>
