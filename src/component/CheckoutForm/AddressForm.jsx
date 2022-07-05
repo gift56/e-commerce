d13fe2a1eeeb357671debafa7cb16ae3d8c8c6b3
gift56 <efeasiughu@gmail.com>
2022-07-05 16:04:13 +0100

    done with the project

--- src/component/CheckoutForm/AddressForm.jsx
+++ src/component/CheckoutForm/AddressForm.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 import FormInput from './CustomTextField';
-console.log(Button);
 
 const AddressForm = ({ checkoutToken }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
@@ -67,6 +67,11 @@ const AddressForm = ({ checkoutToken }) => {
                             </Select>
                         </Grid>
                     </Grid>
+                    <br />
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Button component={Link} to='cart' variant='outlined'>Back to Cart</Button>
+                        <Button type='submit' variant='contained' color='primary'>Next</Button>
+                    </div>
                 </form>
             </FormProvider>
         </>
