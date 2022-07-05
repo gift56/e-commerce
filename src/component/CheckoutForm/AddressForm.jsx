566832049460b0feec16beb9e79151093353a7d8
gift56 <efeasiughu@gmail.com>
2022-07-05 16:16:43 +0100

    removed required

--- src/component/CheckoutForm/AddressForm.jsx
+++ src/component/CheckoutForm/AddressForm.jsx
@@ -44,12 +44,12 @@ const AddressForm = ({ checkoutToken }) => {
             <FormProvider {...methods}>
                 <form onSubmit=''>
                     <Grid container spacing={3}>
-                        <FormInput required name='firstName' label='First Name' />
-                        <FormInput required name='lastName' label='Last Name' />
-                        <FormInput required name='address1' label='Address' />
-                        <FormInput required name='email' label='Email' />
-                        <FormInput required name='city' label='City' />
-                        <FormInput required name='zip' label='Zip / Postal code' />
+                        <FormInput name='firstName' label='First Name' />
+                        <FormInput name='lastName' label='Last Name' />
+                        <FormInput name='address1' label='Address' />
+                        <FormInput name='email' label='Email' />
+                        <FormInput name='city' label='City' />
+                        <FormInput name='zip' label='Zip / Postal code' />
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
                             <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
