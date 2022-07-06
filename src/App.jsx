4395f9d77926bc1c61611103a6fb34fcd7e9c9de
gift56 <efeasiughu@gmail.com>
2022-07-06 12:32:29 +0100

    passed the whole order as a props

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
     const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -42,13 +43,20 @@ const App = () => {
         setCart(cart);
     }
 
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+
+        setCart(newCart)
+    }
+
     const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
         try {
             const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
 
             setOrder(incomingOrder);
+            refreshCart();
         } catch (error) {
-
+            setErrorMessage(error.data.error.message);
         }
     }
 
@@ -71,7 +79,13 @@ const App = () => {
                             handleEmptyCart={handleEmptyCart}
                         />
                     } />
-                    <Route path='/checkout' element={<Checkout cart={cart} />} />
+                    <Route path='/checkout' element={
+                        <Checkout
+                            cart={cart}
+                            order={order}
+                            onCaptureCheckout={handleCaptureCheckout}
+                            error={errorMessage}
+                        />} />
                 </Routes>
             </div>
         </Router>
